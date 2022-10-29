d3a34b8b130f6385d19c97ec1a8525dbc7b38f0e
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2022-10-29 13:14:53 -0600

    Added register auth backend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const app = express();
 const pgp = require("pg-promise")();
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
 const axios = require("axios");
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const socketio = require("socket.io");
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = 3000 || process.env.PORT;
+const url = require('url');
 
 // database configuration
 const dbConfig = {
@@ -56,9 +62,47 @@ app.get('/login', (req, res) => {
   res.status(200).render("pages/login");
 });
 
-io.on("connection", () =>{
- console.log("a user is connected")
+app.get('/register', (req, res) => {
+  res.status(200).render("pages/register", req.query);
 });
 
-app.listen(3000);
-console.log("Server is listening on port 3000");
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const query = "INSERT INTO users(username, password, isAdmin) VALUES ($1, $2, $3)";
+
+  db.task("post/register", async task => {
+    return await task.none(query, [username, hashedPassword, false]);
+  }).then(() => {
+    // Even though format is deprecated, it still works fine and there is no other alternative. Refer to https://github.com/nodejs/node/issues/25099
+    res.redirect(url.format({
+      pathname:"/login",
+      query: {
+        message: "Account successfully created.",
+        error: false,
+        errorMessage: ""
+      }
+    }));
+  }).catch((err) => {
+    console.log(err);
+    if (err.constraint === "users_pkey") {
+      res.status(400).render("pages/register", {
+        message: "The user provided already exists.",
+        error: true,
+        errorMessage: err
+      });
+    } else {
+      res.status(500).render("pages/register", {
+        message: "There was an error inserting to database.",
+        error: true,
+        errorMessage: err
+      });
+    }
+  });
+});
+
+io.on("connection", (socket) => {
+ console.log("A user is connected");
+});
+
+server.listen(3000, () => console.log(`Server is listening on port ${PORT}`));
