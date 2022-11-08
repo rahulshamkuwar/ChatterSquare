08082039bb5407515dd5837ea7efc4c566b2461a
Rahul Shamkuwar <45727404+rahulshamkuwar@users.noreply.github.com>
2022-11-08 00:10:45 -0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,11 @@
 const express = require("express");
-<<<<<<< HEAD
-=======
-const session = require("express-session");
->>>>>>> a44781a (refactor socketio code to be a node service)
 
 const router = express.Router();
 
 // For now redirects to login.
 // TODO: Change to redirect to the square page when it is implemented.
 router.get("/", (req, res) => {
-<<<<<<< HEAD
   res.redirect("/login");
-=======
-  res.redirect("/square");
->>>>>>> a44781a (refactor socketio code to be a node service)
 });
 
 module.exports = router;
