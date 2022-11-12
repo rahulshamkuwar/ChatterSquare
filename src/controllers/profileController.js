d57475c7b88a650be25d7e3dabf5217d77d306d9
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2022-11-11 20:18:59 -0700

    Implemented change password backend

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -19,3 +19,58 @@ exports.profile_get = async (req, res) => {
     }));
   });
 };
+
+exports.profile_post_change_password = async (req, res) => {
+  const { password } = req.body;
+  const newHashedPassword = await bcrypt.hash(password, 10);
+
+  await db.getUser({userId: req.session.user.userId}).then((user) => {
+    const { password: oldHashedPassword } = user;
+    bcrypt.compare(password, oldHashedPassword).then((result) => {
+      if (result) {
+        
+        db.updateUser({userId: req.session.user.userId, password: newHashedPassword}).then(() => {
+          res.status(400).render("pages/profile", {
+            message: "Account successfully created.",
+            pathname: "/profile"
+          });
+        }).catch((err) => {
+          console.log(err);
+          res.status(500).render("pages/profile", {
+            message: "There was an error inserting to database.",
+            error: true,
+            errorMessage: err,
+            pathname: "/profile"
+          });
+        });
+      } else {
+        throw Error("Incorrect password.");
+      }
+    }).catch(err => {
+      console.log(err);
+      res.status(400).render("pages/profile", {
+        message: "Incorrect password.",
+        error: true,
+        errorMessage: err,
+        pathname: "/profile"
+      });
+    });
+  }).catch((err) => {
+    console.log(err);
+    if (err.message === "No data returned from the query.") {
+      res.status(400).render("pages/profile", {
+        message: "Incorrect password.",
+        error: true,
+        errorMessage: err,
+        pathname: "/profile"
+      });
+    } else {
+      res.status(500).render("pages/profile", {
+        message: "There was an error getting the user from database.",
+        error: true,
+        errorMessage: err,
+        pathname: "/profile"
+      });
+    }
+  });
+};
